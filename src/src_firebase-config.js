02b3b6c95c548527eda25c74ada959ac7b0cbe25
AhnurInc <guga.lefebvre@gmail.com>
2025-07-16 18:02:16 -0300

    Add files via upload

---
+++ src/src_firebase-config.js
@@ -0,0 +1,15 @@
+// src/firebase-config.js
+
+// Lembre-se: Pegue esta configuração do SEU NOVO projeto Firebase.
+// NUNCA compartilhe este arquivo com as chaves preenchidas.
+const firebaseConfig = {
+  apiKey: "SUA_NOVA_API_KEY",
+  authDomain: "SEU_NOVO_AUTH_DOMAIN",
+  projectId: "SEU_NOVO_PROJECT_ID",
+  storageBucket: "SEU_NOVO_STORAGE_BUCKET",
+  messagingSenderId: "SEU_NOVO_MESSAGING_SENDER_ID",
+  appId: "SEU_NOVO_APP_ID"
+};
+
+// Inicializa o Firebase
+// (Vamos adicionar o código de inicialização aqui no próximo passo)
