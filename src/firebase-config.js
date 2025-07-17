b9d2fd9e463263439611c210068ecf9371897723
AhnurInc <guga.lefebvre@gmail.com>
2025-07-17 13:14:47 -0300

    Adiciona export ao firebase-config

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,8 +1,6 @@
-// src/firebase-config.js
-
-// Lembre-se: Pegue esta configuração do SEU NOVO projeto Firebase.
-// NUNCA compartilhe este arquivo com as chaves preenchidas.
-const firebaseConfig = {
+// A MUDANÇA CRÍTICA ESTÁ AQUI: Adicionamos a palavra 'export'.
+// Este é o "selo" que permite que outros arquivos importem esta variável.
+export const firebaseConfig = {
   apiKey: "AIzaSyDC2-Gyr6448WW2IAAdJwoqeXTq8d_Wq9Y",
   authDomain: "ahnur-two.firebaseapp.com",
   projectId: "ahnur-two",
@@ -10,6 +8,3 @@ const firebaseConfig = {
   messagingSenderId: "589270591243",
   appId: "1:589270591243:web:ba395a6ae003d582b2240e"
 };
-
-// Inicializa o Firebase
-// (Vamos adicionar o código de inicialização aqui no próximo passo)
